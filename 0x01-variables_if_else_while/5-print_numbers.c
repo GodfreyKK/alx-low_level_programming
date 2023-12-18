2c8aa0a5cebe51072277db815c2be45cb1cf5fb8
GodfreyKK <godfreykevink@gmail.com>
2023-12-18 07:08:21 -0800

    1 -9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print 1 - 9
+ * Return: 0
+ */
+int main(void)
+{
+	char A;
+
+	for (A = '0'; A <= '9'; A++)
+	{
+		putchar(A);
+	}
+	putchar('\n');
+	return (0);
+}
