a6879473eb407aa98f8f7f5ce12c5f65abe1b041
GodfreyKK <godfreykevink@gmail.com>
2023-12-18 07:28:30 -0800

    using ASCII

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print 1 - 9
+ * Return: 0
+ */
+int main(void)
+{
+	int A;
+
+	for (A = 48; A <= 57; A++)
+	{
+		putchar(A);
+	}
+	putchar('\n');
+	return (0);
+}
