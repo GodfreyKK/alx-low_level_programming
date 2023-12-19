f17762056613c7d36da82f2dd767fe0802a3acb3
GodfreyKK <godfreykevink@gmail.com>
2023-12-19 11:09:53 -0800

    void print_alphabet(void);

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char A;
+
+	for (A = 'a'; A <= 'z'; A++)
+	{
+		_putchar(A);
+	}
+	_putchar('\n');
+}
