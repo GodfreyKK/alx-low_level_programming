b298e4c5625973db29b0544205080ad8949c413b
GodfreyKK <godfreykevink@gmail.com>
2023-12-21 11:08:48 -0800

    removed the excess statements and turned it to =

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,15 +12,15 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if ((a > b && a > c) && (b < a && c < a))
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if ((b > a && b > c) && (a < b && c < b))
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
-	else if ((c > a && c > b) && (a < c && b < c))
+	else
 	{
 		largest = c;
 	}
