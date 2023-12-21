bf550720e2c08a14fc0b48c9424e5cddb67fde58
GodfreyKK <godfreykevink@gmail.com>
2023-12-21 12:10:34 -0800

    added _isupper declaration to the comments as well as eddited the lower to upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _islower - check if a character is upper
+ * _isupper - check if a character is upper
  * @c: character to be checked
- * Return: 1 if lower and 0 for otherwise
+ * Return: 1 if upper and 0 for otherwise
  */
 int _isupper(int c)
 {
