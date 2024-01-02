b527c91d71daaa09ea53d88b5d299cec8ca8a1c6
GodfreyKK <godfreykevink@gmail.com>
2024-01-02 07:45:38 -0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - print a string in reverse
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int count = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+		count++;
+	}
+	for (i = count - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
