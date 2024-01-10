0d9caabd429ae338b5fafcff8b261e070332a4a9
GodfreyKK <godfreykevink@gmail.com>
2024-01-10 04:26:54 -0800

    add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - addition of two integers
+ * @x: variable name of type int
+ * @y: variable name of type int
+ * Return: sum
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
