960f8db8f5b5e35095be4825cf441101ea50b714
GodfreyKK <godfreykevink@gmail.com>
2024-01-11 08:15:06 -0800

    printing a line on terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,16 @@ void print_line(int n)
 {
 	int i;
 
-	if (n == 0)
-	{
-		_putchar('_');
-	}
-	else
-	{
 	for (i = 1; i <= n; i++)
 	{
-		_putchar('_');
-	}
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
 	}
 	_putchar('\n');
 }
