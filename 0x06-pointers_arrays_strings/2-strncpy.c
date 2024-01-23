debc2f495e3d48020a3b2ba0cf0d515eeaf8db37
GodfreyKK <godfreykevink@gmail.com>
2024-01-23 07:32:49 -0800

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy n bytes from src to dest
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to be copied from src
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && dest[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
