e4dc7f2d73d521bdf3de82b772ac1c04d51c9e2f
GodfreyKK <godfreykevink@gmail.com>
2024-02-14 01:13:19 -0800

    print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * print_name - a fuction that takes a char and a function pointer
+ * @name: first argument
+ * @f: second argument
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	void (*ptr)(char *);
+
+	ptr = f;
+	ptr(name);
+}
