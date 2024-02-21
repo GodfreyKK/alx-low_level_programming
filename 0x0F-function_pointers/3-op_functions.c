da313a44366827cbd496421577e1199757823311
GodfreyKK <godfreykevink@gmail.com>
2024-02-21 14:48:06 -0800

    added comments

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -3,10 +3,6 @@
 #include <stdio.h>
 /**
  * op_add - add a and b and return value
- * op_sub - subtract a and b and return value
- * op_mul - multiply both a and b and return value
- * op_div - dived a by b and return value
- * op_mod - check the remaider of a by b and return value
  *
  * @a: first argument
  * @b: second argument
@@ -18,14 +14,40 @@ int op_add(int a, int b)
 {
 return (a + b);
 }
+/**
+ * op_sub - subtract a and b and return value
+ *
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: int
+ */
 int op_sub(int a, int b)
 {
 return (a - b);
 }
+/**
+ * op_mul - multiply both a and b and return value
+ *
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: int
+ */
+
 int op_mul(int a, int b)
 {
 return (a * b);
 }
+/**
+ * op_div - dived a by b and return value
+ *
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: int
+ */
+
 int op_div(int a, int b)
 {
 if (b == 0)
@@ -35,6 +57,15 @@ exit(100);
 }
 return (a / b);
 }
+/**
+ * op_mod - check the remaider of a by b and return value
+ *
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: int
+ */
+
 int op_mod(int a, int b)
 {
 if (b != 0)
