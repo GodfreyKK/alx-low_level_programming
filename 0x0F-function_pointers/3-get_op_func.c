53c4b689ec9c9107bb8ec03faacf04a74d14f87f
GodfreyKK <godfreykevink@gmail.com>
2024-02-21 14:38:35 -0800

    removed trailing spaces

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -11,21 +11,21 @@ int (*get_op_func(char *s))(int, int)
 {
 	int i = 0;
 
-	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-	while (ops[i].op != NULL)
-	{
-		if (ops[i].op[0] == *s && *(s + 1) == '\0')
-		{
-			return (ops[i].f);
-		}
-		i++;
-	}
-	return (NULL);
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+while (ops[i].op != NULL)
+{
+if (ops[i].op[0] == *s && *(s + 1) == '\0')
+{
+return (ops[i].f);
+}
+i++;
+}
+return (NULL);
 }
