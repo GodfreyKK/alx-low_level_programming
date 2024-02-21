28e9ed24b7fe9f46fc8fc3e382227b263c3ff082
GodfreyKK <godfreykevink@gmail.com>
2024-02-21 14:31:27 -0800

    added comments corrected space on parenthesis

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,34 +1,46 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * op_add - add a and b and return value
+ * op_sub - subtract a and b and return value
+ * op_mul - multiply both a and b and return value
+ * op_div - dived a by b and return value
+ * op_mod - check the remaider of a by b and return value
+ *
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: int
+ */
 
 int op_add(int a, int b)
 {
-        return (a + b);
+return (a + b);
 }
 int op_sub(int a, int b)
 {
-        return (a - b);
+return (a - b);
 }
 int op_mul(int a, int b)
 {
-        return (a * b);
+return (a * b);
 }
 int op_div(int a, int b)
 {
-        if ( b == 0)
-        {
-                printf("Error\n");
-                exit(100);
-        }
-        return (a / b);
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
+return (a / b);
 }
 int op_mod(int a, int b)
 {
-        if (b != 0)
-        {
-                return (a % b);
-        }
-        printf("Error\n");
-        exit(100);
+if (b != 0)
+{
+return (a % b);
+}
+	printf("Error\n");
+	exit(100);
 }
