077eff28bb0c126bed659c3c7265f5d9dfb7d495
GodfreyKK <godfreykevink@gmail.com>
2024-02-21 11:11:40 -0800

    added a check ststement for when eithe s1 or s2 or both are NULL

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,17 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, len1 = 0, len2 = 0;
 	char *cAt;
 
-	if (s2 == NULL || s1 == NULL)
+	if (s2 == NULL && s1 == NULL)
 	{
-		return (0);
+		return (NULL);
+	}
+	if (s1 == NULL && s2 != NULL)
+	{
+		return (s2);
+	}
+	if (s2 == NULL && s1 != NULL)
+	{
+		return (s1);
 	}
 	while (s1[i] != '\0')
 	{
