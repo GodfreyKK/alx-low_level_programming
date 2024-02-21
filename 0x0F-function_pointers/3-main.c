962fa89969942a1eae0b6f9800b2bf66aa052c44
GodfreyKK <godfreykevink@gmail.com>
2024-02-21 14:18:19 -0800

    removed white spaces and chrected pointer in the main argument

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,35 +2,36 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include "3-calc.h"
-/** 
+/**
  * main - function
  * @argc: argument count
  * @argv: argument vector
  * Return: int
  */
 
-int main (int argc, char* argv[])
-{       
-        int num1, num2, calc;
-        int (*y)(int, int);
-        
-        if (argc != 4)
-        {       
-                printf("Error\n");
-                exit(98);
-        }
+int main(int argc, char *argv[])
+{
+
+	int num1, num2, calc;
+	int (*y)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-        y = get_op_func(argv[2]);
+	y = get_op_func(argv[2]);
 	if (y == NULL)
-        {
-                printf("Error\n");
-                exit(99);
-        }
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	calc = y(num1, num2);
 
 	printf("%d\n", calc);
-	return (0);
+return (0);
 }
